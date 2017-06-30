58cd3167bc9d763d9c476dfd5658d58c38194313
Kay Krewson <kay.k@comcast.net>
2017-06-30 08:04:21 -0700

    removed console.logs and cleaned-up array-search algorithms file

--- src/array-search/search-algorithms.js
+++ src/array-search/search-algorithms.js
@@ -1,8 +1,8 @@
 'use strict';
+let testArray1 = [4,8,9,22,115,600,601,606,777]
 
 /*exports.linearSearch = */
 function linearSearch(array, val) {
-
   array.forEach(function(el, index) {
     if (el === val) {
       console.log("found the value, it is at index ", index)
@@ -13,28 +13,50 @@ function linearSearch(array, val) {
   return -1
 }
 
-/*exports.binarySearch = */
-function binarySearch(array, value, min = 0, max = array.length) {
-let arrayLength = array.length
-let midpoint = arrayLength?Math.ceil(arrayLength/2):0
+exports.binarySearch = function(array, value, min = 0, max = array.length) {
+  let length = array.length
+  let midpoint = length?Math.ceil(length/2):0
 
-while (min <= max) {
-  if (value === array[midpoint]) {
-    console.log('Found the value. It is at index ', midpoint)
-    return value
-  } else if (value < array[midpoint]) {
-    max = midpoint-1
-    arrayLength = max-min
-    midpoint = arrayLength?Math.ceil(arrayLength/2):0
-  } else {
-    min = midpoint+1
-    arrayLength = max-min
-    midpoint = min + (arrayLength?Math.ceil(arrayLength/2):0)
-  }
-} // while
-console.log('item not found.')
-return
+  while (min <= max) {
+    if (value === array[midpoint]) {
+      console.log('Found the value. It is at index ', midpoint)
+      return value
+    } else if (value < array[midpoint]) {
+      max = midpoint-1
+      length = max-min
+      midpoint = length?Math.ceil(length/2):0
+    } else {
+      min = midpoint+1
+      length = max-min
+      midpoint = min + (length?Math.ceil(length/2):0)
+    }
+  } // while
+  console.log('item not found.')
+  return
 }
-let testArray1 = [4,8,9,22,115,600,601,606,777]
+
+// ===== BINARY SEARCH =====
+function binarySearch(array, value, min = 0, max = array.length) {
+  let length = array.length
+  let midpoint = length?Math.ceil(length/2):0
+
+  while (min <= max) {
+    if (value === array[midpoint]) {
+      console.log('Found the value. It is at index ', midpoint)
+      return value
+    } else if (value < array[midpoint]) {
+      max = midpoint-1
+      length = max-min
+      midpoint = length?Math.ceil(length/2):0
+    } else {
+      min = midpoint+1
+      length = max-min
+      midpoint = min + (length?Math.ceil(length/2):0)
+    }
+  } // while
+  console.log('item not found.')
+  return
+}
+
 // binarySearch(testArray1, 606)
-linearSearch(testArray1, 5)
+// linearSearch(testArray1, 5)
