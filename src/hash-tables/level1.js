c9ff3d6e522d15c982aaf66bec13d546d149712b
Kay Krewson <kay.k@comcast.net>
2017-07-06 17:42:01 -0700

    removeDupes working

--- src/hash-tables/level1.js
+++ src/hash-tables/level1.js
@@ -1,19 +1,5 @@
 'use strict';
 
-/*
-
-Will Smith is back in the music game and his PR team is telling him he should make his former pop
-hit, 'Miami', multi-lingual. Below is a list of languages his team want him to include. Will is a busy
-guy so he has asked you to help him out.
-
-Create a funtion named greet()
-It takes a single argument 'language'
-It will return 'Welcome to Miami.' where 'Welcome' will be in the language specified.
-
-i.e  when the language is latvian, the fn will return 'Gaidits to Miami.'
-
-*/
-
 const LANG_LIST = {
   english: 'Welcome',
   czech: 'Vitejte',
@@ -41,16 +27,25 @@ const greet = (language) => {
 /*
 Create a function named removeDupes.
 It takes a single argument 'numArr' of type Array.
-It will return a hash who's keys are value of the item in the array.
+It will return a hash who's keys are the value of the item in the array.
 The hash's value will be the item value's index in the array.
 If there are more than one occurance, the index set to the hash should be of the last occurance.
 
 i.e. [1, 'z', 'bob', true, 42, 'bob'] => { '1': 0, 'z': 1, 'bob': '5', 'true': 3, '42': 4 }
 */
 
+// ===== REMOVEDUPES =====
 const removeDupes = (numArr) => {
-
-};
+  if (numArr.length) {
+    let size = numArr.length
+    let hashTable = {}
+    for (let i = 0; i < size; i++) {
+      hashTable[numArr[i]] = i
+    }
+    return hashTable
+  }
+  return {}
+}
 
 /*
 
@@ -62,9 +57,10 @@ i.e. { 'foo': 6, 'bar': 3, 'baz': 'bob', 13: 13 } => { 'bar': 3, '13': 13 }
 
 */
 
+// ===== ONLYODDS =====
 const onlyOdds = (mixedHash) => {
 
-};
+}
 
 /*
 
@@ -81,3 +77,17 @@ const charCount = (word) => {
 };
 
 module.exports = { greet, removeDupes, onlyOdds, charCount };
+
+/*
+
+Will Smith is back in the music game and his PR team is telling him he should make his former pop
+hit, 'Miami', multi-lingual. Below is a list of languages his team want him to include. Will is a busy
+guy so he has asked you to help him out.
+
+Create a funtion named greet()
+It takes a single argument 'language'
+It will return 'Welcome to Miami.' where 'Welcome' will be in the language specified.
+
+i.e  when the language is latvian, the fn will return 'Gaidits to Miami.'
+
+*/
